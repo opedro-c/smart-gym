fab396c362885e5b85b50e14a7cd4a64e634d550
Pedro Costa Aragão <pedro.aragao.labcomu@imd.ufrn.br>
2024-10-31 21:11:34 -0300

    feat(#1): wip on go client setup

--- gym/cmd/main.go
+++ gym/cmd/main.go
@@ -2,8 +2,38 @@ package main
 
 import (
 	"fmt"
+	"os"
+	MQTT "github.com/eclipse/paho.mqtt.golang"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	opts := MQTT.NewClientOptions()
+	opts.AddBroker("tcp://localhost:1883")
+	opts.SetClientID("1")
+	opts.SetUsername("admin")
+	opts.SetPassword("admin")
+	opts.SetCleanSession(false)
+
+	receiveCount := 0
+	choke := make(chan [2]string)
+	opts.SetDefaultPublishHandler(func(client MQTT.Client, msg MQTT.Message) {
+		choke <- [2]string{msg.Topic(), string(msg.Payload())}
+	})
+
+	client := MQTT.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	qos := 0
+	if token := client.Subscribe("/hello", byte(qos), nil); token.Wait() && token.Error() != nil {
+		fmt.Println(token.Error())
+		os.Exit(1)
+	}
+
+	for receiveCount < 2 {
+		incoming := <-choke
+		fmt.Printf("RECEIVED TOPIC: %s MESSAGE: %s\n", incoming[0], incoming[1])
+		receiveCount++
+	}
 }
