30fb8382918951ea96e585e54fd0d80f2ee28d33
Pedro Costa Aragão <pedro@MacBook-Pro-de-Pedro.local>
2025-01-10 22:59:50 -0300

    feat: read distance to count sets and reps

---
+++ machine/main.ino
@@ -0,0 +1,38 @@
+void setup() {
+  // put your setup code here, to run once:
+  ExercisePayload test;
+  Serial.begin(115200);
+  Serial.println("Hello, ESP32!");
+  pinMode(PIN_TRIG, OUTPUT);
+  pinMode(PIN_ECHO, INPUT);
+
+  repTimerHandle = xTimerCreate(
+    "repTimer",
+    pdMS_TO_TICKS(TIMEOUT_CURRENT_SET_MS),
+    pdFALSE,
+    (void *) 0,
+    finishCurrentSet
+  );
+
+  setTimerHandle = xTimerCreate(
+    "setTimer",
+    pdMS_TO_TICKS(TIMEOUT_CURRENT_EXERCISE_MS),
+    pdFALSE,
+    (void *) 0,
+    finishCurrentExercise
+  );
+
+  xTaskCreate(
+    countNumberOfRepetitions,
+    "countReps",
+    4094,
+    NULL,
+    1,
+    NULL
+  );
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(10); // this speeds up the simulation, remove later
+}
